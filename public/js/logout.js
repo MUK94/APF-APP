4ea90aab4a147110b1757138546b46dde4512bd1
Mouctar <mklecodeur@gmail.com>
2022-04-28 15:41:48 +0300

    Added trycatch block to logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,12 +1,16 @@
 // @ts-nocheck
 /* eslint-disable */
 const logout = async () => {
-  const res = await axios({
-    method: 'GET',
-    url: '/api/v1/users/logout'
-  });
-  if ((res.data.status = 'success'))
-    location.reload(true), location.assign('/');
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: '/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success'))
+      location.reload(true), location.assign('/');
+  } catch (error) {
+    alert('error', 'Error Logging out! Try again');
+  }
 };
 
 document.querySelector('.logOut').addEventListener('click', logout);
